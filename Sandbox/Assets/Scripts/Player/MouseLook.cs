3c4bcc42c0afa30639668c2ee5cfce19f9a3dad7
FilipMargitai <filip.margitai@pslib.cz>
2020-12-27 11:45:53 +0100

    mouse look

---
+++ Sandbox/Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [HideInInspector]
+    public float mouseX;
+    [HideInInspector]
+    public float mouseY;
+
+    public float mouseSensitivity = 500f;
+
+    private float xRotation = 0f;
+    [HideInInspector]
+    public Transform playerBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = transform.parent.transform;
+        Cursor.lockState = CursorLockMode.Locked; //Cursor stays in the middle of the screen
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseInput();
+        BodyRotation();
+    }
+    private void MouseInput()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+    }
+    private void BodyRotation()
+    {
+        //Rotation of X
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //You can't rotate around by looking up or down
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+
+        //Rotation of Y
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
