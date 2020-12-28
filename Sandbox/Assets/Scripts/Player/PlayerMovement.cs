15fa2c5486bed916226d8eea0b46749caabb75da
FilipMargitai <filip.margitai@pslib.cz>
2020-12-28 15:08:31 +0100

    character movement

---
+++ Sandbox/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float x;
+
+    private float z;
+
+    private CharacterController controller;
+
+    public float speed = 10f;
+
+    private Vector3 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyboardInput();
+        BodyMovement();
+    }
+    private void KeyboardInput()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+    }
+    private void BodyMovement()
+    {
+        move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
